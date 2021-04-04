0744061d94aa309b0417619a41caedbbe09b3dcf
Brian Jenkins <brianjenkins94@gmail.com>
2021-04-03 21:13:43 -0400

    Logging

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import createAuth0Client from "@auth0/auth0-spa-js"; // "@auth0/auth0-spa-js/src/index";
 
-let user;
-
 document.addEventListener("DOMContentLoaded", async function(event) {
 
 	const auth0Client = await createAuth0Client({
@@ -10,10 +8,10 @@ document.addEventListener("DOMContentLoaded", async function(event) {
 		"redirect_uri": location.origin + location.pathname
 	});
 
-	user = await auth0Client.getUser();
+	const user = await auth0Client.getUser();
 
 	document.getElementById("login").addEventListener("click", function() {
-		console.log(user);
+		console.log("`login` clicked.");
 
 		auth0Client.loginWithRedirect().catch(function(error) {
 			alert(error);
@@ -21,23 +19,19 @@ document.addEventListener("DOMContentLoaded", async function(event) {
 	});
 
 	document.getElementById("logout").addEventListener("click", function() {
-		console.log(user);
+		console.log("`logout` clicked.");
 
 		auth0Client.logout();
 	});
 
-	document.getElementById("getInfo").addEventListener("click", async function() {
-		console.log(user);
+	document.getElementById("getInfo").addEventListener("click", function() {
+		console.log("`getInfo` clicked.");
 
-		document.body.innerHTML += JSON.stringify(user, undefined, 4);
+		document.body.append(JSON.stringify(user, undefined, 4));
 	});
 
-	auth0Client.handleRedirectCallback().then(async function(redirectResult) {
-
+	auth0Client.handleRedirectCallback().then(function(redirectResult) {
 		console.log("Logged in.");
-
-		user = await auth0Client.getUser();
-
 	});
 
 });
